0e9f894be5f980576be2730c8cf33c522d133de4
root <root@DESKTOP-33BFR3H>
2025-05-01 17:31:23 +0900

    add the accounts customer and hotel app

--- backend/customer/models.py
+++ backend/customer/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django_countries.fields import CountryField
+from django_countries.fields import CountryField  # type: ignore
 
 
 User = get_user_model()
