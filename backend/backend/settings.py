1196f501539a0430512bbe43a4acf5706e4807cb
Jonam Adhikari <jadhikari131@gmail.com>
2025-05-27 23:10:14 +0900

    chnage in nginx file and setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,3 +162,6 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "test@example.com")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
+CSRF_TRUSTED_ORIGINS = ['https://sys.fishtail.jp']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
